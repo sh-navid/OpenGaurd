950f666511df491eabc0ca2ea1934c553f044d78
sh-navid <shamshirsaz.navid@live.com>
2024-02-17 14:53:54 +0330

    Setup java build1

---
+++ build/java/Test1.java
@@ -0,0 +1,16 @@
+
+class Test1{
+    public static void main(String[] argv){
+        String _FS9YDP3GU1T3 = "our";
+        final String _VYPKRD77ZHDM = "counter";
+        System.out.println(String.format("This is %s ",_FS9YDP3GU1T3)+_VYPKRD77ZHDM);
+        int _PJUJ9BRX1HOC=12;
+        _PJUJ9BRX1HOC+=1;
+        boolean _YTQ3YNGBKVGJ=true;
+        _YTQ3YNGBKVGJ=!_YTQ3YNGBKVGJ;
+
+        for (int _Q40BHXVOUMWE=10;_Q40BHXVOUMWE<15;_Q40BHXVOUMWE++ ) {
+            System.out.println(String.format("\n$_FS9YDP3GU1T3 %d ",_Q40BHXVOUMWE)+_PJUJ9BRX1HOC + String.format("|%b",_YTQ3YNGBKVGJ));
+        }
+    }
+}
