197ca46da9dbc3ea2d30777d7b3cc39f9a5359a0
sh-navid <shamshirsaz.navid@live.com>
2024-02-17 09:15:09 +0330

    Setup js test1

---
+++ build/js/Test1.js
@@ -0,0 +1,15 @@
+
+const main = () => {
+  const v1 = "our"
+  let v2 = "counter";
+  console.log(`This is ${v1} ` + v2);
+  var _4D4PW012LID2 = 12
+  _4D4PW012LID2 += 1
+  var _OO6QGC9WQMWI = true
+  _OO6QGC9WQMWI = !_OO6QGC9WQMWI;
+
+  for (k = 10; k <= 15; k++) {
+    console.log(`\n${v1} ${k} ` + _4D4PW012LID2 + `|${_OO6QGC9WQMWI}`);
+  }
+};
+main()
